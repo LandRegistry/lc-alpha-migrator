f75ee1ccd12a72f609179fc11a45996704d611d5
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:45:44 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ class Config(object):
     DEBUG = False
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     DEBUG = True
+    B2B_LEGACY_URL = "http://localhost:5007"
+    BANKRUPTCY_DATABASE_API = "http://localhost:5004"
+
+
+class PreviewConfig(Config):
     B2B_LEGACY_URL = "http://localhost:5007"
     BANKRUPTCY_DATABASE_API = "http://localhost:5004"
