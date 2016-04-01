14aad104b38d07fd2632a194ab38afc661fc5d81
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-04-01 12:21:54 +0100

    Switch to single env vars

--- config.py
+++ config.py
@@ -4,14 +4,10 @@ import os
 class Config(object):
     APPLICATION_NAME = "lc-migrator"
     DEBUG = os.getenv('DEBUG', True)
-    MQ_USERNAME = os.getenv("MQ_USERNAME", "mquser")
-    MQ_PASSWORD = os.getenv("MQ_PASSWORD", "mqpassword")
-    MQ_HOSTNAME = os.getenv("MQ_HOST", "localhost")
-    MQ_PORT = os.getenv("MQ_PORT", "5672")
+    SQLALCHEMY_DATABASE_URI = "postgresql://landcharges:lcalpha@localhost/landcharges"
+    AMQP_URI = os.getenv("AMQP_URI", "amqp://mquser:mqpassword@localhost:5672")
+    PSQL_CONNECTION = os.getenv("PSQL_CONNECTION", "dbname='landcharges' user='landcharges' host='localhost' password='lcalpha'")
+
     LEGACY_ADAPTER_URI = os.getenv('LEGACY_ADAPTER_URL', 'http://10.0.2.2:15007')
     #LEGACY_ADAPTER_URI = os.getenv('LEGACY_ADAPTER_URL', 'http://localhost:5007')
     LAND_CHARGES_URI = os.getenv('LAND_CHARGES_URL', 'http://localhost:5004')
-    DATABASE_NAME = os.getenv('DATABASE_NAME', 'landcharges')
-    DATABASE_USER = os.getenv('DATABASE_USER', 'landcharges')
-    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'lcalpha')
-    DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
